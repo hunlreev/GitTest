0dfd14a17c0cbe6a160085dd78facfce14ff450d
hunlreev <hunlreev@ut.utm.edu>
2018-11-27 16:23:16 -0600

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,29 @@
 
 using namespace std;
 
+int sum(int n);
+
 int main() {
+
+  int n;
   
   cout << "Hello World!" << endl;
+  cout << "Enter a number: ";
+  cin >> n;
+
+  cout << "The summed value is: " << sum(n) << endl;
   
   return 0;
+}
+
+int sum(int n) {
+
+  int sum = 0;
+
+  for (int i = 0; i <= n; i++) {
+
+    sum += i;
+  }
+
+  return sum;
 }
